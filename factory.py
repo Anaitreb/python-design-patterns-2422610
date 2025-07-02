bc59d06355136445a13ea9520bf7954bb2ece0f2
Anaitreb <b.balliu@yahoo.it>
2025-07-02 14:53:33 +0000

    modified first factory

--- factory.py
+++ factory.py
@@ -7,3 +7,33 @@ class Dog:
 
 	def speak(self):
 		return "Woof!"
+	
+def get_pet(pet="dog"):
+	
+	"""The Factory Method"""
+
+	pets = dict(dog=Dog("Hope"))
+	return pets[pet]
+
+class Cat:
+
+	"""A simple cat class"""
+
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return "Meow!"
+	
+def get_pet(pet="cat"):
+	
+	"""The Factory Method"""
+
+	pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+	return pets[pet]
+
+d =get_pet("dog")
+print(d.speak())	
+
+c = get_pet("cat")
+print(c.speak())
